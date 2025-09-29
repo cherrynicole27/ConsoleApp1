10b93db1d0649b7a593862b0751e81c650d15aa2
cherrynicole27 <fernandezcherrynicole05@gmail.com>
2025-09-29 16:27:54 +0800

    Reverted to the former version

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World again!!!");
-Console.WriteLine("Hello, World again and again!!!");
-
-Console.WriteLine("This is a demo for the Fetch feauture of Github");
-
-
